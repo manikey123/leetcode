1585343a886df15911421c47f6f1fc7524042c61
manikey123 <mansiimohan@gmail.com>
2023-12-03 10:53:28 -0800

    two sum

--- python/0001-two-sum.py
+++ python/0001-two-sum.py
@@ -1,3 +1,4 @@
+from typing import List 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         prevMap = {}  # val -> index
@@ -7,3 +8,10 @@ class Solution:
             if diff in prevMap:
                 return [prevMap[diff], i]
             prevMap[n] = i
+s = Solution()
+nums  = [2,7,11,15] ; target = 9
+nums2 = [3,2,4]; target2 = 6
+nums3 = [3,3] ; target3 = 6
+tuple = (  (nums,target) , (nums2,target2) , (nums3,target3) )
+for item in tuple:
+    print("input:",item, "result:", s.twoSum(item[0], item[1]))
